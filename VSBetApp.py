c370db39b0110e7a49c3c4261dbee22abda058d0
BK024 <bart.berkel@gmail.com>
2019-08-25 22:06:45 +0200

    Started working on the table to show a bet.

--- VSBetApp.py
+++ VSBetApp.py
@@ -1,11 +1,20 @@
 from flask import Flask
-
+from PollScraper import getDemCandidatePollData as getDemData
 
 app = Flask(__name__)
 
+def makeTableString(name, amt, candidate, perc):
+    pass
+
+
 @app.route("/")
 def main():
-    return "Welcome!"
+    returnString = \
+    """
+    ---Name----------Sum (euro)-------Candidate------
+    ---{}{}{}------
+    """
+    return str(getDemData())
 
 
 
